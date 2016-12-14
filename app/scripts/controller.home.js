6606192ba96d25f421a05331211fcd880fbae1c8
Senthil Kumar M <senthilmpro@Senthils-MacBook-Pro.local>
2016-12-15 00:08:09 +0530

    wordpress changes

--- app/scripts/controller.home.js
+++ app/scripts/controller.home.js
@@ -85,7 +85,7 @@ app.controller('dpHomeCtrl', ['$scope','service.sites','service.util','settings'
 
 
 	$scope.getWPAuth = function(){
-		var authUrl = "https://public-api.wordpress.com/oauth2/authorize?client_id=51005&redirect_uri=https://desipixer.github.io/dp-grunt/dist/#/home&response_type=token";
+		var authUrl = "https://public-api.wordpress.com/oauth2/authorize?client_id=51005&redirect_uri=https://desipixer.github.io&response_type=token";
 		$http.get(authUrl).success(function(obj){
 			console.log(obj);
 		}).error(function(err){
